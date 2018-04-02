5c708b50e992671498350d8e7dacc22ec5d6af80
向世明 <1666181800@qq.com>
2018-04-02 14:28:41 +0800

    first commit

---
+++ js/jibaiJump.js
@@ -0,0 +1,34 @@
+  //打开家谱页面
+
+document.getElementById('jinianguan').addEventListener('tap', function() {
+  //打开纪念馆页面
+  mui.openWindow({
+    url: 'jinianguan.html', 
+    id:'jinianguan'
+  });
+});
+
+
+document.getElementById('jiapu').addEventListener('tap', function() {
+  //打开家谱页面
+  mui.openWindow({
+    url: 'jiapu.html', 
+    id:'jiapu'
+  });
+});
+
+document.getElementById('xingshi').addEventListener('tap', function() {
+  //打开姓氏起源页面
+  mui.openWindow({
+    url: 'xingshi.html', 
+    id:'xingshi'
+  });
+});
+
+document.getElementById('my').addEventListener('tap', function() {
+  //打开个人中心页面
+  mui.openWindow({
+    url: 'my.html', 
+    id:'my'
+  });
+});
